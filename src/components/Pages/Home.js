bbf9766b4a259d67dea25762ce250c4101ebead1
Kevin Chu <kvnchu2@gmail.com>
2022-11-20 17:54:04 -0800

    add home.js landing page and sections, and also add css

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,57 @@
 import React from "react";
+import './Home.css';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <section id="hero">
+          <div class="hero-content">
+              <div class="heroText">
+                  <h1 id="home-h">Main headline, put top keyword here </h1>
+                  <p>
+                      Expand on the keyword with more keywords and add a list of related services that you do and include the city, state of your service area.  
+                  </p>
+                  <div class="buttons">
+                      <a class="button-solid" href="/about.html" >
+                          About Us
+                      </a>
+                      <a class="call-now" href="tel:555-213-9120">
+                          <img class="light" decoding="async" src="../../images/phone.svg" alt="" aria-hidden="true" width="21" height="21"></img>
+                          <img class="dark" decoding="async" src="../../images/phone-dark.svg" alt="" aria-hidden="true" width="21" height="21"></img>
+                          <div class="group">
+                              <span class="text">Call Now</span>
+                              <span class="number">(555) 213-9120</span>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+              <img decoding="async" src="../../images/ryan-caselli.png" alt="" class="business-casual" width="" height=""></img>
+          </div>
+      </section>
+
+    <section id="services" class="section">
+        <div class="service">
+            <h2>This is a main<span> service you do</span></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio dui, fermentum id augue.
+            </p>
+            <a href="/contact.html" class="button-solid">Get in Touch</a>
+        </div>
+        <div class="service">
+            <h2>This is THE main<span> service you do</span></h2>
+            <p>
+                Notice how this one is bigger and darker, this is your main service you provide.
+            </p>
+            <a href="/contact.html" class="button-solid">Get in Touch</a>
+        </div>
+        <div class="service">
+            <h2>This is a main<span> service you do</span></h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi odio dui, fermentum id augue .
+            </p>
+            <a href="/contact.html" class="button-solid">Get in Touch</a>
+        </div>
+    </section>
+    </>
   );
 };
